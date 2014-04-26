a006ba8474fc41235161f5dd16ce1f5d461deaa3
Daniel Zigerlig <dzigerli@hsr.ch>
2014-04-26 11:35:02 +0200

    removed directive

--- WebContent/cdarclient/app/js/appDirectives.js
+++
@@ -1,9 +0,0 @@
-angular.module('app.directives.navigation', []).directive('cdarNavigation', function() {
-	return {
-		restrict: 'E',
-		templateUrl: 'directives/navigation.html',
-		controller: function() {
-			console.log('cdar navigation');
-		}
-	};
-});
