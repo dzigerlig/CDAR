de8d891fff27ffa102c3462509d5075e23e9c4cd
mtinner <mtinner@hsr.ch>
2014-04-28 19:38:05 +0200

    add nodelinkcontroller and subnodecontroller

--- src/main/java/cdar/pl/controller/KnowledgeTreeController.java
+++ src/main/java/cdar/pl/controller/KnowledgeTreeController.java
@@ -1,8 +1,5 @@
 package cdar.pl.controller;
 
-import java.util.List;
-import java.util.Set;
-
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -13,18 +10,10 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import cdar.bll.CDAR_Boolean;
-import cdar.bll.CDAR_BooleanChanges;
 import cdar.bll.producer.Directory;
-import cdar.bll.producer.Node;
-import cdar.bll.producer.NodeLink;
-import cdar.bll.producer.Subnode;
 import cdar.bll.producer.Template;
 import cdar.bll.producer.Tree;
-import cdar.bll.producer.XmlTree;
 import cdar.bll.producer.models.DirectoryModel;
-import cdar.bll.producer.models.NodeLinkModel;
-import cdar.bll.producer.models.NodeModel;
-import cdar.bll.producer.models.SubnodeModel;
 import cdar.bll.producer.models.TemplateModel;
 import cdar.bll.producer.models.TreeModel;
 import cdar.bll.producer.models.XmlTreeModel;
@@ -32,9 +21,7 @@ import cdar.bll.producer.models.XmlTreeModel;
 @Path("ktree")
 public class KnowledgeTreeController {
 	private TreeModel ktm = new TreeModel();
-	private NodeLinkModel lm = new NodeLinkModel();
 	private DirectoryModel dm = new DirectoryModel();
-	private SubnodeModel sm = new SubnodeModel();
 	private TemplateModel tm = new TemplateModel();
 	private XmlTreeModel xtm = new XmlTreeModel();
 
@@ -275,211 +262,9 @@ public class KnowledgeTreeController {
 	}
 
 	
-	// Links
-	@GET
-	// Changed
-	@Path("{ktreeid}/links")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getLinks(@PathParam("ktreeid") int ktreeid) {
-		try {
-			return Response.ok(lm.getNodeLinks(ktreeid),
-					MediaType.APPLICATION_JSON).build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-	}
+	
 
-	@POST
-	// Changed
-	@Path("{ktreeid}/links/add")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response addNodeLink(NodeLink nl) {
-		try {
-			return Response.ok(
-					lm.addNodeLink(nl.getKtrid(), nl.getSourceId(),
-							nl.getTargetId(), nl.getKsnid()),
-					MediaType.APPLICATION_JSON).build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.UNAUTHORIZED).build();
-		}
-	}
-
-	@POST
-	// Changed
-	@Path("{ktreeid}/links/delete")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response deleteNodeLink(int id) {
-		try {
-			return Response.ok(new CDAR_Boolean(lm.deleteNodeLink(id)),
-					MediaType.APPLICATION_JSON).build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-	}
-
-	@POST
-	// Changed
-	@Path("{ktreeid}/links/update")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response updateNodeLink(NodeLink nl) {
-		try {
-			return Response.ok(lm.updateNodeLink(nl),
-					MediaType.APPLICATION_JSON).build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.UNAUTHORIZED).build();
-		}
-	}
-
-	@GET
-	// Changed
-	@Path("{ktreeid}/links/{nodeid}/zoomUp")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response zoomUpLink(@PathParam("nodeid") int nodeid) {
-		try {
-			return Response.ok(lm.zoomUp(nodeid), MediaType.APPLICATION_JSON)
-					.build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-	}
-
-	@GET
-	// Changed
-	@Path("{ktreeid}/links/{nodeid}/zoomDown")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response zoomDownLink(@PathParam("nodeid") int nodeid) {
-		try {
-			return Response.ok(lm.zoomDown(nodeid), MediaType.APPLICATION_JSON)
-					.build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-	}
-
-	// Subnodes
-	@GET
-	// Changed
-	@Path("{ktreeid}/subnodes")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getSubnodesByTree(@PathParam("ktreeid") int ktreeid) {
-		try {
-			return Response.ok(sm.getSubnodesFromTree(ktreeid),
-					MediaType.APPLICATION_JSON).build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-	}
-
-	@GET
-	// Changed
-	@Path("{ktreeid}/subnodes/{nodeid}")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getSubnodes(@PathParam("nodeid") int nodeid) {
-		try {
-			return Response.ok(sm.getSubnodesFromNode(nodeid),
-					MediaType.APPLICATION_JSON).build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-	}
-
-	@POST
-	// Changed
-	@Path("{ktreeid}/subnodes/rename")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response renameSubnode(Subnode subnode) {
-		try {
-			sm.renameSubnode(subnode);
-			return Response
-					.ok(new CDAR_BooleanChanges<NodeLink>(true, lm
-							.getNodeLinksBySubnode(subnode.getId()), "update"),
-							MediaType.APPLICATION_JSON).build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.UNAUTHORIZED).build();
-		}
-	}
-
-	@POST
-	// Changed
-	@Path("{ktreeid}/subnodes/moveup")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response moveSubnodeUp(Subnode subnode) {
-		try {
-			return Response.ok(new CDAR_Boolean(sm.moveSubnodeUp(subnode)),
-					MediaType.APPLICATION_JSON).build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.UNAUTHORIZED).build();
-		}
-	}
-
-	@POST
-	// Changed
-	@Path("{ktreeid}/subnodes/movedown")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response moveSubnodeDown(Subnode subnode) {
-		try {
-			return Response.ok(new CDAR_Boolean(sm.moveSubnodeDown(subnode)),
-					MediaType.APPLICATION_JSON).build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.UNAUTHORIZED).build();
-		}
-	}
-
-	@POST
-	// Changed
-	@Path("{ktreeid}/subnodes/add")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response addSubnode(Subnode sn) {
-		try {
-			return Response.ok(sm.addSubnode(sn.getKnid(), sn.getTitle()),
-					MediaType.APPLICATION_JSON).build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.UNAUTHORIZED).build();
-		}
-	}
-
-	@POST
-	// Changed
-	@Path("{ktreeid}/subnodes/delete/")
-	@Consumes(MediaType.APPLICATION_JSON)
-	public Response deleteSubnode(int id) {
-		try {
-			List<NodeLink> nodelinks = lm.getNodeLinksBySubnode(id);
-
-			return Response.ok(
-					new CDAR_BooleanChanges<NodeLink>(sm.deleteSubnode(id),
-							nodelinks, "delete"), MediaType.APPLICATION_JSON)
-					.build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-	}
-
-	@GET
-	// Changed
-	@Path("{ktreeid}/subnodes/{nodeid}/zoomUp")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response zoomUpSubnode(@PathParam("nodeid") int nodeid) {
-		try {
-			return Response.ok(sm.zoomUp(nodeid), MediaType.APPLICATION_JSON)
-					.build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-	}
-
-	@GET
-	// Changed
-	@Path("{ktreeid}/subnodes/{nodeid}/zoomDown")
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response zoomDownSubnode(@PathParam("nodeid") int nodeid) {
-		try {
-			return Response.ok(sm.zoomDown(nodeid), MediaType.APPLICATION_JSON)
-					.build();
-		} catch (Exception e) {
-			return Response.status(Response.Status.BAD_REQUEST).build();
-		}
-	}
+	
 
 	// TREE XML
 	@GET
