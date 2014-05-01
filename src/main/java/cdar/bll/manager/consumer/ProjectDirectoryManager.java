837952450203376902bf6f6d2aadc5b6ee172c2d
Daniel Zigerlig <dzigerli@hsr.ch>
2014-05-01 11:19:19 +0200

    added ProjectDirectoryManager

---
+++ src/main/java/cdar/bll/manager/consumer/ProjectDirectoryManager.java
@@ -0,0 +1,54 @@
+package cdar.bll.manager.consumer;
+
+import java.sql.SQLException;
+import java.util.HashSet;
+import java.util.Set;
+
+import cdar.bll.entity.Directory;
+import cdar.dal.consumer.ProjectDirectoryRepository;
+import cdar.dal.exceptions.UnknownDirectoryException;
+
+public class ProjectDirectoryManager {
+	private ProjectDirectoryRepository pdr = new ProjectDirectoryRepository();
+
+	public Set<Directory> getDirectories(int treeId) throws SQLException {
+		Set<Directory> directories = new HashSet<Directory>();
+		for (Directory directory : pdr.getDirectories(treeId)) {
+			directories.add(directory);		
+		}
+		return directories;
+	}
+	
+	public Directory getDirectory(int directoryId) throws UnknownDirectoryException {
+		return pdr.getDirectory(directoryId);
+	}
+
+	public boolean deleteDirectory(int directoryId) throws Exception {
+		return pdr.deleteDirectory(directoryId);
+	}
+
+	public Directory addDirectory(int treeId, int parentId, String title) throws Exception	
+	{ 
+		Directory directory = new Directory();
+		directory.setTreeId(treeId);
+		directory.setParentId(parentId);
+		directory.setTitle(title);
+		return pdr.createDirectory(directory);
+	}
+
+	public Directory renameDirectory(Directory directory) throws Exception {
+		Directory updatedDirectory = getDirectory(directory.getId());
+		updatedDirectory.setTitle(directory.getTitle());
+		return pdr.updateDirectory(updatedDirectory);
+	}
+	
+	public Directory updateDirectory(Directory directory) throws Exception {
+		return pdr.updateDirectory(directory);
+	}
+
+	public Directory moveDirectory(Directory directory) throws Exception {
+		Directory movedDirectory = pdr.getDirectory(directory.getId());
+		movedDirectory.setParentId(directory.getParentId());
+		return pdr.updateDirectory(movedDirectory);
+	}
+}
