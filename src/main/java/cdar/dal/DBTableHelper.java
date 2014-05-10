1f433e8b2c09a773785caacbe18eaebc7314d4d3
mtinner <mtinner@Pavilion.home>
2014-05-10 09:33:22 +0200

    add static table helper class

---
+++ src/main/java/cdar/dal/DBTableHelper.java
@@ -0,0 +1,25 @@
+package cdar.dal;
+
+public class  DBTableHelper {
+	public static final String USER = "User";
+	
+private static final String TEMPLATE = "KnowledgeTemplate"; 
+private static final String TREE = "KnowledgeTree";
+private static final String DIRECTORY = "Directory"; 
+private static final String NODE = "KnowledgeNode"; 
+private static final String NODELINK = "NodeLink"; 
+private static final String SUBNODE = "KnowledgeSubnode"; 
+private static final String TREEMAPPING = "KnowledgeTreeMapping"; 
+private static final String NODEMAPPING = "KnowledgeNodeMapping"; 
+private static final String TREEXML = 	"KnowledgeTreXml"; 
+
+private static final String PROJECTTREE = "KnowledgeProjectTree"; 
+private static final String PROJECTDIRECTORY ="KnowledgeProjectDirectory"; 
+private static final String PROJECTNODE = "KnowledgeProjectNode"; 
+private static final String PROJECTSUBNODE = "KnowledgeProjectSubnode"; 
+private static final String PROJECTNODELINK = "KnowledgeProjectNodeLink"; 
+private static final String PROJECTTREEMAPPING = "KnowledgeProjectTreeMapping"; 
+private static final String PROJECTNODEMAPPING = "KnowledgeProjectNodeMapping"; 
+private static final String USERCOMMENT = "UserComment"; 
+private static final String PROJECTTREEXML = "ProjectTreeXml"; 
+}
