b3d5db7b6bec208010cae9c9af66b33e2920fea8
Daniel Zigerlig <dzigerli@hsr.ch>
2014-05-26 20:32:20 +0200

    javadoc: locking

--- src/main/java/cdar/bll/entity/Locking.java
+++ src/main/java/cdar/bll/entity/Locking.java
@@ -5,58 +5,105 @@ import java.util.Date;
 
 import cdar.dal.helpers.PropertyHelper;
 
+/**
+ * Class used for locking, to prevent data overrides when multiple users are trying to change the same tree at the same time
+ * @author dani
+ *
+ */
 public class Locking {	
 	private int userId;
 	private int treeId;
 	private boolean isProducer;
 	private Date lockingTime;
 	
+	/**
+	 * Default constructor
+	 */
 	public Locking() {
 		super();
 	}
 
+	/**
+	 * @param userId id of the user who is editing (int)
+	 * @param treeId id of the tree which the user is trying to edit (int)
+	 * @param isProducer as a boolean to check if the user is in role producer or consumer
+	 */
 	public Locking(int userId, int treeId, boolean isProducer) {
 		super();
 		setUserId(userId);
 		setTreeId(treeId);
 		setProducer(isProducer);
-		setLockingTime(lockingTime = new Date());
+		setLockingTime(new Date());
 		renewLockingTime();
 	}
 
-
+	/**
+	 * 
+	 * @return id of the user as int
+	 */
 	public int getUserId() {
 		return userId;
 	}
 
+	/**
+	 * 
+	 * @param userId as int to be set
+	 */
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
 
+	/**
+	 * 
+	 * @return id of the tree as int
+	 */
 	public int getTreeId() {
 		return treeId;
 	}
 
+	/**
+	 * 
+	 * @param treeId as int to be set
+	 */
 	public void setTreeId(int treeId) {
 		this.treeId = treeId;
 	}
 
+	/**
+	 * 
+	 * @return boolean value representing the user state, true if user is in role producer, false if user is in role consumer
+	 */
 	public boolean isProducer() {
 		return isProducer;
 	}
 
+	/**
+	 * 
+	 * @param isProducer boolean value, true if user is in role producer, false if user is in role consumer
+	 */
 	public void setProducer(boolean isProducer) {
 		this.isProducer = isProducer;
 	}
 
+	/**
+	 * 
+	 * @return locking time as a Date value
+	 */
 	public Date getLockingTime() {
 		return lockingTime;
 	}
 
+	/**
+	 * 
+	 * @param lockingTime new date value of the locking time to be set
+	 */
 	public void setLockingTime(Date lockingTime) {
 		this.lockingTime = lockingTime;
 	}
 
+	/**
+	 * Gets the time of a lock out of the property file and adds it to the current date
+	 */
 	public void renewLockingTime() {
 		PropertyHelper property = new PropertyHelper();
 		Calendar calendar = Calendar.getInstance();
@@ -66,6 +113,10 @@ public class Locking {
 		setLockingTime(calendar.getTime());
 	}
 
+	/**
+	 * 
+	 * @return boolean value which tells if the lock is expired (true) or not (false)
+	 */
 	public boolean isExpired() {
 		return getLockingTime().before(new Date());
 	}
