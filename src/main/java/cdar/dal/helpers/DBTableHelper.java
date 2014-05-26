55e540a5000a94515023a2c514001f2178ecb045
Daniel Zigerlig <dzigerli@hsr.ch>
2014-05-26 13:38:11 +0200

    changed database table names

--- src/main/java/cdar/dal/helpers/DBTableHelper.java
+++ src/main/java/cdar/dal/helpers/DBTableHelper.java
@@ -3,23 +3,23 @@ package cdar.dal.helpers;
 public class DBTableHelper {
 	public static final String USER = "User";
 
-	public static final String TEMPLATE = "KnowledgeTemplate";
-	public static final String TREE = "KnowledgeTree";
+	public static final String TEMPLATE = "Template";
+	public static final String TREE = "Tree";
 	public static final String DIRECTORY = "Directory";
-	public static final String NODE = "KnowledgeNode";
+	public static final String NODE = "Node";
 	public static final String NODELINK = "NodeLink";
-	public static final String SUBNODE = "KnowledgeSubnode";
-	public static final String TREEMAPPING = "KnowledgeTreeMapping";
-	public static final String NODEMAPPING = "KnowledgeNodeMapping";
-	public static final String TREEXML = "KnowledgeTreeXml";
+	public static final String SUBNODE = "Subnode";
+	public static final String TREEMAPPING = "TreeMapping";
+	public static final String NODEMAPPING = "NodeMapping";
+	public static final String TREEXML = "TreeXml";
 
-	public static final String PROJECTTREE = "KnowledgeProjectTree";
-	public static final String PROJECTDIRECTORY = "KnowledgeProjectDirectory";
-	public static final String PROJECTNODE = "KnowledgeProjectNode";
-	public static final String PROJECTSUBNODE = "KnowledgeProjectSubnode";
-	public static final String PROJECTNODELINK = "KnowledgeProjectNodeLink";
-	public static final String PROJECTTREEMAPPING = "KnowledgeProjectTreeMapping";
-	public static final String PROJECTNODEMAPPING = "KnowledgeProjectNodeMapping";
+	public static final String PROJECTTREE = "ProjectTree";
+	public static final String PROJECTDIRECTORY = "ProjectDirectory";
+	public static final String PROJECTNODE = "ProjectNode";
+	public static final String PROJECTSUBNODE = "ProjectSubnode";
+	public static final String PROJECTNODELINK = "ProjectNodeLink";
+	public static final String PROJECTTREEMAPPING = "ProjectTreeMapping";
+	public static final String PROJECTNODEMAPPING = "ProjectNodeMapping";
 	public static final String USERCOMMENT = "UserComment";
 	public static final String PROJECTTREEXML = "ProjectTreeXml";
 }
